e56b6968aec89e426a2a324221fe321bae393abc
samuckqadev <samuel.tomimatu@bandtec.com.br>
2023-07-13 21:25:41 -0300

    first commit

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+// This is  default theme to use in project
+type ThemeType = typeof defaultTheme
+
+// This create a defaltTheme module
+declare module 'styled-components' {
+  export type DefaultTheme = ThemeType
+}
